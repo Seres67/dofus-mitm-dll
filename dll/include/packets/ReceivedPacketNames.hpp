35a8aec1c2fb400da301ce94910164c3ad1782ab
Laurent <seres@seres.eu.org>
2024-05-02 12:08:20 +0200

    added sent & received packets names & ids classes

---
+++ dll/include/packets/ReceivedPacketNames.hpp
@@ -0,0 +1,22 @@
+//
+// Created by lou on 02/05/2024.
+//
+
+#ifndef DOFUS_TEST_RECEIVEDPACKETNAMES_HPP
+#define DOFUS_TEST_RECEIVEDPACKETNAMES_HPP
+
+
+#include <unordered_map>
+#include <string>
+
+class ReceivedPacketNames {
+public:
+    static void setup_map();
+    static std::string get_packet_name(uint16_t packet_id);
+
+private:
+    static std::unordered_map<uint16_t, std::string> m_packet_names;
+};
+
+
+#endif //DOFUS_TEST_RECEIVEDPACKETNAMES_HPP
--- dll/src/main.cpp
+++ dll/src/main.cpp
@@ -20,6 +20,8 @@
 #include "packets/impl/UnknownPacket.hpp"
 #include "packets/impl/BasicPingPacket.hpp"
 #include "packets/factory/PacketFactory.hpp"
+#include "packets/ReceivedPacketNames.hpp"
+#include "packets/SentPacketNames.hpp"
 
 const std::vector<uint16_t> unknown_ids = {
 
@@ -33,11 +35,6 @@ std::ofstream log_file;
 using sendFnc = int (WSAAPI *)(SOCKET, const char *, int, int);
 using recvFnc = int (WSAAPI *)(SOCKET, char *, int, int);
 
-std::ostream &operator<<(std::ostream &os, const APacket &a) {
-    a.dump();
-    return os;
-}
-
 template<typename ReturnType, typename FunctionType>
 ReturnType PassthroughHook(void *caller, SOCKET socket, char *buffer, int length, int flags) {
     auto original{(FunctionType) HookEngine::GetOriginalAddressFromHook(caller)};
@@ -50,52 +47,13 @@ ReturnType PassthroughHook(void *caller, SOCKET socket, char *buffer, int length
 }
 
 int WSAAPI sendHook(SOCKET socket, const char *buffer, int length, int flags) {
-//    sendGlobalBuffer.insert(sendGlobalBuffer.end(), buffer, buffer + length);
-    uint16_t packet_id = (uint8_t) buffer[0] << 8 | (uint8_t) buffer[1];
-    packet_id >>= 2;
-    std::cout << "send " << packet_id << ": ";
-    if (packet_id == std::to_underlying(PacketIds::ChatMessage)) {
-        ChatMessagePacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::MapMovement)) {
-        MapMovementPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::GameContextRemoveElement)) {
-        GameContextRemoveElementPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::MapNoMovement)) {
-        NoMovementPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::MapFightCount)) {
-        MapFightCountPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::MultiTabStorage)) {
-        MultiTabStoragePacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::BasicAck)) {
-        BasicAckPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::PlayerInventory)) {
-        PlayerInventoryPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (packet_id == std::to_underlying(PacketIds::Pong)) {
-        BasicPongPacket packet(buffer);
-        std::cout << packet << std::endl;
-    } else if (std::find(unknown_ids.begin(), unknown_ids.end(), packet_id) != unknown_ids.end()) {
-        std::cout << std::endl;
-    } else {
-        std::cout << "unimplemented packet id" << std::endl;
-    }
-    return PassthroughHook<int, sendFnc>(sendHook, socket, const_cast<char *>(buffer), length, flags);
-}
-
-int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
-    if (length <= 5)
-        return PassthroughHook<int, recvFnc>(recvHook, socket, buffer, length, flags);
+//    if (length <= 5)
+//        return PassthroughHook<int, sendFnc>(sendHook, socket, const_cast<char *>(buffer), length, flags);
     if (length != 65536) {
-        UnknownPacket packet(buffer);
-        std::cout << "Small{id: " << packet.getPacketId() << "; lent:" << +packet.getLengthType() << " ;plen: " << packet.getLength() << "}"
-                  << std::endl;
+        std::unique_ptr<IPacket> packet = PacketFactory::make_packet(buffer);
+//        std::cout << "sending " << length << " bytes, header: " << packet->getHeader() << ", id: " << packet->getPacketId() << std::endl;
+        std::string packet_dump = packet->dump(true);
+        std::cout << "sending: " << packet_dump << std::endl;
     } else {
         uint32_t pos = 0;
         uint32_t t_pos;
@@ -122,7 +80,53 @@ int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
             }
             if (looks_valid) {
                 std::unique_ptr<IPacket> packet = PacketFactory::make_packet(&buffer[pos]);
-                std::cout << packet->dump() << std::endl;
+                std::string packet_dump = packet->dump(true);
+                if (!packet_dump.empty())
+                    std::cout << "sending: " << packet_dump << std::endl;
+            }
+            pos = t_pos;
+        }
+    }
+    return PassthroughHook<int, sendFnc>(sendHook, socket, const_cast<char *>(buffer), length, flags);
+}
+
+int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
+    if (length <= 5)
+        return PassthroughHook<int, recvFnc>(recvHook, socket, buffer, length, flags);
+    if (length != 65536) {
+        std::unique_ptr<IPacket> packet = PacketFactory::make_packet(buffer);
+        std::string packet_dump = packet->dump(false);
+        if (!packet_dump.empty())
+            std::cout << "receiving: " << packet_dump << std::endl;
+    } else {
+        uint32_t pos = 0;
+        uint32_t t_pos;
+        while (pos < length) {
+            t_pos = pos;
+            UnknownPacket _packet(&buffer[t_pos]);
+            bool looks_valid = false;
+            t_pos += 3;
+            if (_packet.getPacketId() < 10 || _packet.getPacketId() >= 10000 || _packet.getLengthType() == 0 || _packet.getLength() == 0) {
+                pos = t_pos;
+                continue;
+            }
+            if (_packet.getLengthType() == 1 && _packet.getLength() < 256) {
+                t_pos += _packet.getLength();
+                looks_valid = true;
+            }
+            if (_packet.getLengthType() == 2 && _packet.getLength() > 256 && _packet.getLength() < 65536) {
+                t_pos += _packet.getLength();
+                looks_valid = true;
+            }
+            if (_packet.getLengthType() == 3 && _packet.getLength() > 65536 && _packet.getLength() < 16777216) {
+                t_pos += _packet.getLength();
+                looks_valid = true;
+            }
+            if (looks_valid) {
+                std::unique_ptr<IPacket> packet = PacketFactory::make_packet(&buffer[pos]);
+                std::string packet_dump = packet->dump(false);
+                if (!packet_dump.empty())
+                    std::cout << "receiving: " << packet_dump << std::endl;
             }
             pos = t_pos;
         }
@@ -136,6 +140,8 @@ WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, [[maybe_unused]] LPVOID reserv
 
     if (dwReason == DLL_PROCESS_ATTACH) {
         log_file.open("./log.txt", std::ios_base::binary);
+        ReceivedPacketNames::setup_map();
+        SentPacketNames::setup_map();
         DisableThreadLibraryCalls(hModule);
         if (AllocConsole()) {
             freopen_s((FILE **) stdout, "CONOUT$", "w", stdout);
@@ -146,13 +152,13 @@ WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, [[maybe_unused]] LPVOID reserv
             std::cerr << "DLL Loaded" << std::endl;
         }
 
-//        (void) hookEngine.Hook("Ws2_32.dll", "send", sendHook);
+        (void) hookEngine.Hook("Ws2_32.dll", "send", sendHook);
         (void) hookEngine.Hook("Ws2_32.dll", "recv", recvHook);
     }
 
     if (dwReason == DLL_PROCESS_DETACH) {
         log_file.close();
-//        (void) hookEngine.Unhook(std::string_view("Ws2_32.dll"), std::string_view("send"));
+        (void) hookEngine.Unhook(std::string_view("Ws2_32.dll"), std::string_view("send"));
         (void) hookEngine.Unhook(std::string_view("Ws2_32.dll"), std::string_view("recv"));
     }
 
