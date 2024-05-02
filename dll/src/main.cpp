1e652c51b7b979b6a09deea1ac8be532ddb58559
Laurent <seres@seres.eu.org>
2024-05-02 23:20:33 +0200

    testing to parse packets on another thread

--- dll/src/main.cpp
+++ dll/src/main.cpp
@@ -4,13 +4,15 @@
 #include <algorithm>
 #include <sstream>
 #include <fstream>
+#include <mutex>
+#include <deque>
 #include "HookEngine.h"
 #include "packets/impl/ChatMessagePacket.hpp"
 #include "packets/impl/UnknownPacket.hpp"
 #include "packets/factory/PacketFactory.hpp"
 #include "packets/PacketNames.hpp"
 
-std::ofstream log_file;
+//std::ofstream log_file;
 
 using sendFnc = int (WSAAPI *)(SOCKET, const char *, int, int);
 using recvFnc = int (WSAAPI *)(SOCKET, char *, int, int);
@@ -32,7 +34,7 @@ int WSAAPI sendHook(SOCKET socket, const char *buffer, int length, int flags) {
         std::string packet_dump = packet->dump();
         if (!packet_dump.empty()) {
             std::cout << "sending: " << packet_dump << std::endl;
-            log_file << "sending: " << packet_dump << std::endl;
+//            log_file << "sending: " << packet_dump << std::endl;
         }
     } else {
         uint32_t pos = 0;
@@ -62,7 +64,7 @@ int WSAAPI sendHook(SOCKET socket, const char *buffer, int length, int flags) {
                 std::unique_ptr<IPacket> packet = PacketFactory::make_packet(&buffer[pos]);
                 std::string packet_dump = packet->dump();
                 if (!packet_dump.empty()) {
-                    log_file << "sending: " << packet_dump << std::endl;
+//                    log_file << "sending: " << packet_dump << std::endl;
                     std::cout << "sending: " << packet_dump << std::endl;
                 }
             }
@@ -72,50 +74,138 @@ int WSAAPI sendHook(SOCKET socket, const char *buffer, int length, int flags) {
     return PassthroughHook<int, sendFnc>(sendHook, socket, const_cast<char *>(buffer), length, flags);
 }
 
-int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
-    if (length <= 5)
-        return PassthroughHook<int, recvFnc>(recvHook, socket, buffer, length, flags);
-    if (length != 65536) {
-        std::unique_ptr<IPacket> packet = PacketFactory::make_packet(buffer);
-        std::string packet_dump = packet->dump();
-        if (!packet_dump.empty()) {
-            std::cout << "receiving: " << packet_dump << std::endl;
-            log_file << "receiving: " << packet_dump << std::endl;
-        }
-    } else {
-        uint32_t pos = 0;
-        uint32_t t_pos;
-        while (pos < length) {
-            t_pos = pos;
-            UnknownPacket _packet(&buffer[t_pos]);
-            bool looks_valid = false;
-            t_pos += 3;
-            if (_packet.getPacketId() < 10 || _packet.getPacketId() >= 10000 || _packet.getLengthType() == 0 || _packet.getLength() == 0) {
-                pos = t_pos;
-                continue;
+std::vector<char> recvBuffer;
+std::mutex bufferMutex;
+bool running = true;
+
+void processDataAsync() {
+    while (running) {
+        while (recvBuffer.size() >= 5) {
+            // get packet header
+            std::vector<char> tmp;
+            {
+                std::lock_guard<std::mutex> lock(bufferMutex);
+                tmp = std::vector(recvBuffer.begin(), recvBuffer.begin() + 5);
             }
-            if (_packet.getLengthType() == 1 && _packet.getLength() < 256) {
-                t_pos += _packet.getLength();
-                looks_valid = true;
-            }
-            if (_packet.getLengthType() == 2 && _packet.getLength() > 256 && _packet.getLength() < 65536) {
-                t_pos += _packet.getLength();
-                looks_valid = true;
-            }
-            if (_packet.getLengthType() == 3 && _packet.getLength() > 65536 && _packet.getLength() < 16777216) {
-                t_pos += _packet.getLength();
-                looks_valid = true;
-            }
-            if (looks_valid) {
-                std::unique_ptr<IPacket> packet = PacketFactory::make_packet(&buffer[pos]);
-                std::string packet_dump = packet->dump();
-                if (!packet_dump.empty()) {
-                    std::cout << "receiving: " << packet_dump << std::endl;
-                    log_file << "receiving: " << packet_dump << std::endl;
+            uint64_t m_pos = 0;
+            uint16_t m_header = (uint8_t) tmp[m_pos] << 8 | (uint8_t) tmp[m_pos + 1];
+            uint16_t m_packet_id = m_header >> 2;
+            uint8_t m_length_type = m_header & 3;
+            m_pos += 2;
+            // check if packet id is valid
+            if (PacketNames::get_packet_name(m_packet_id).empty()) {
+                {
+                    std::lock_guard<std::mutex> lock(bufferMutex);
+                    recvBuffer.erase(recvBuffer.begin());
                 }
+                break;
             }
-            pos = t_pos;
+            // check if length type is correct
+            if (m_length_type == 0 || m_length_type > 3) {
+                {
+                    std::lock_guard<std::mutex> lock(bufferMutex);
+                    recvBuffer.erase(recvBuffer.begin());
+                    std::cout << "invalid length type" << std::endl;
+                }
+                break;
+            }
+            // get packet data length
+            uint32_t m_length = 0;
+            if (m_length_type == 1) {
+                m_length = (uint8_t) tmp[m_pos];
+            } else if (m_length_type == 2)
+                m_length = (uint8_t) tmp[m_pos] << 8 | (uint8_t) tmp[m_pos + 1];
+            else if (m_length_type == 3)
+                m_length = (uint8_t) tmp[m_pos] << 16 | (uint8_t) tmp[m_pos + 1] << 8 | (uint8_t) tmp[m_pos + 2];
+            // check if length is valid
+            if (m_length == 0) {
+                {
+                    std::lock_guard<std::mutex> lock(bufferMutex);
+                    recvBuffer.erase(recvBuffer.begin());
+                    std::cout << "invalid length" << std::endl;
+                }
+                break;
+            }
+            if ((m_length_type == 1 && m_length < 256) || (m_length_type == 2 && m_length >= 256 && m_length < 65536) ||
+                (m_length_type == 3 && m_length >= 65536 && m_length < 16777216)) {
+            } else {
+                {
+                    std::lock_guard<std::mutex> lock(bufferMutex);
+                    recvBuffer.erase(recvBuffer.begin());
+                }
+                break;
+            }
+            //check if buffer has enough data
+            if (recvBuffer.size() < m_length + 3)
+                break;
+            std::cout << "valid " << m_header << " " << m_packet_id << " " << +m_length_type << " len:" << m_length
+                      << std::endl;
+            //get packet data
+            std::unique_ptr<char[]> buffer(new char[m_length + 3]);
+            {
+                std::lock_guard<std::mutex> lock(bufferMutex);
+                memcpy_s(buffer.get(), m_length + 3, recvBuffer.data(), m_length + 3);
+                recvBuffer.erase(recvBuffer.begin(), recvBuffer.begin() + m_length + 2);
+            }
+            //parse packet data
+            std::unique_ptr<IPacket> packet = PacketFactory::make_packet(buffer.get());
+            std::cout << "received " << packet->dump() << std::endl;
         }
+    }
+}
+
+
+int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
+//    if (length < 5)
+//        return PassthroughHook<int, recvFnc>(recvHook, socket, buffer, length, flags);
+//    if (length != 65536) {
+//        std::unique_ptr<IPacket> packet = PacketFactory::make_packet(buffer);
+//        std::string packet_dump = packet->dump();
+//        if (!packet_dump.empty()) {
+//            std::cout << "receiving: " << packet_dump << std::endl;
+//            log_file << "receiving: " << packet_dump << std::endl;
+//        }
+//    } else {
+//        uint32_t pos = 0;
+//        uint32_t t_pos;
+//        while (pos < length) {
+//            t_pos = pos;
+//            UnknownPacket _packet(&buffer[t_pos]);
+//            bool looks_valid = false;
+//            t_pos += 3;
+//            if (_packet.getPacketId() < 10 || _packet.getPacketId() >= 10000 || _packet.getLengthType() == 0 || _packet.getLength() == 0) {
+//                pos = t_pos;
+//                continue;
+//            }
+//            if (_packet.getLengthType() == 1 && _packet.getLength() < 256) {
+//                t_pos += _packet.getLength();
+//                looks_valid = true;
+//            }
+//            if (_packet.getLengthType() == 2 && _packet.getLength() >= 256 && _packet.getLength() < 65536) {
+//                t_pos += _packet.getLength();
+//                looks_valid = true;
+//            }
+//            if (_packet.getLengthType() == 3 && _packet.getLength() >= 65536 && _packet.getLength() < 16777216) {
+//                t_pos += _packet.getLength();
+//                looks_valid = true;
+//            }
+//            if (looks_valid) {
+//                std::unique_ptr<IPacket> packet = PacketFactory::make_packet(&buffer[pos]);
+//                std::string packet_dump = packet->dump();
+//                if (!packet_dump.empty()) {
+//                    std::cout << "receiving: " << packet_dump << std::endl;
+//                    log_file << "receiving: " << packet_dump << std::endl;
+//                }
+//            }
+//            pos = t_pos;
+//        }
+//    }
+    uint16_t m_header = (uint8_t) buffer[0] << 8 | (uint8_t) buffer[1];
+    uint16_t m_packet_id = m_header >> 2;
+    std::cout << "main thread: " << m_header << " " << m_packet_id << " " + PacketNames::get_packet_name(m_packet_id) << std::endl;
+    {
+        std::lock_guard<std::mutex> lock(bufferMutex);
+        recvBuffer.insert(recvBuffer.end(), buffer, buffer + length);
     }
     return PassthroughHook<int, recvFnc>(recvHook, socket, buffer, length, flags);
 }
@@ -123,9 +213,10 @@ int WSAAPI recvHook(SOCKET socket, char *buffer, int length, int flags) {
 [[maybe_unused]] __declspec(dllexport) BOOL
 WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, [[maybe_unused]] LPVOID reserved) {
     static HookEngine hookEngine{};
+    std::thread dataProcessingThread(processDataAsync);
 
     if (dwReason == DLL_PROCESS_ATTACH) {
-        log_file.open("./log.txt", std::ios_base::binary);
+//        log_file.open("./log.txt", std::ios_base::binary);
         PacketNames::setup_map();
         DisableThreadLibraryCalls(hModule);
         if (AllocConsole()) {
@@ -142,7 +233,9 @@ WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, [[maybe_unused]] LPVOID reserv
     }
 
     if (dwReason == DLL_PROCESS_DETACH) {
-        log_file.close();
+//        log_file.close();
+        running = false;
+        dataProcessingThread.join();
         (void) hookEngine.Unhook(std::string_view("Ws2_32.dll"), std::string_view("send"));
         (void) hookEngine.Unhook(std::string_view("Ws2_32.dll"), std::string_view("recv"));
     }
